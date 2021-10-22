0ff483b72832f39c4a2fdef19927a3685b0303f2
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2021-10-22 14:17:15 +0100

    Add project files.

---
+++ Training/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.IO;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string st = File.ReadAllText(@"C:\AlarmList.tsv");
+                Console.WriteLine(st);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
